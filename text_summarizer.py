646d1825523dfca919f4dae8ff97fe56b44478c6
soumyasucharitagh <150057513+soumyasucharitagh@users.noreply.github.com>
2026-03-13 23:55:58 +0530

    Update text_summarizer.py

--- text_summarizer.py
+++ text_summarizer.py
@@ -1,23 +1,16 @@
-import streamlit as st
 from transformers import pipeline
 
-@st.cache_resource
-def load_model():
-    summarizer = pipeline(
-        "summarization",
-        model="facebook/bart-large-cnn"
-    )
-    return summarizer
-
+# Load once (important for Streamlit performance)
+summarizer = pipeline(
+    "summarization",
+    model="facebook/bart-large-cnn"
+)
 
 def summarize_text(text, max_length=150, min_length=40):
-    summarizer = load_model()
-
     summary = summarizer(
         text,
         max_length=max_length,
         min_length=min_length,
         do_sample=False
     )
-
-    return summary[0]["summary_text"]
+    return summary[0]['summary_text']
